f831b4ad0a58e8d3c2c36128a9fc8479473b264d
Ashutosh Tripathi <ashu.tripathi0817@gmail.com>
2026-04-17 16:12:50 +0530

    Updated Security

---
+++ src/main/java/com/booking/flight/Security/JwrAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.booking.flight.Security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwrAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("{\"error\":\"Unauthorized\"}");
+    }
+}
